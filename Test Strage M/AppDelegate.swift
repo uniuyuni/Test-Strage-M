1c969ac05fdd6633528c0c0c854db167b44052aa
uniuyuni <uniuyuni@gmail.com>
2020-08-25 07:56:17 +0900

    Initial Commit

---
+++ Test Strage M/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Test Strage M
+//
+//  Created by うに on 2020/08/25.
+//  Copyright © 2020 うに. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Test Strage M/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Test Strage M
+//
+//  Created by うに on 2020/08/25.
+//  Copyright © 2020 うに. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
