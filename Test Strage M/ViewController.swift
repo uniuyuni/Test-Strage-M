e1f023087b0bfecadce3b1f3c8eed98592d30ba4
uniuyuni <uniuyuni@gmail.com>
2020-09-02 17:21:55 +0900

    1.0 release

--- Test Strage M/ViewController.swift
+++ Test Strage M/ViewController.swift
@@ -8,6 +8,9 @@
 
 import Cocoa
 
+let MAX_FILE_SIZE: Int = 2*1024*1024*1024/16    // 作成ファイルの最大サイズ
+
+
 class ViewController: NSViewController, USBDetectorDelegate {
 
     @IBOutlet weak var volumeList: NSPopUpButton!
@@ -18,6 +21,7 @@ class ViewController: NSViewController, USBDetectorDelegate {
     @IBOutlet weak var dontDeleteFilesCheck: NSButton!
     @IBOutlet weak var deleteButton: NSButton!
     @IBOutlet weak var stateText: NSTextField!
+    @IBOutlet weak var verifyButton: NSButton!
     
     typealias StatFS = statfs // this does the trick
 
@@ -31,7 +35,7 @@ class ViewController: NSViewController, USBDetectorDelegate {
         init( url: URL, volumeName: String, dispString: String ) {
             var fs: StatFS = StatFS()
             if statfs(url.path, &fs) == 0 {
-                self.path = url.path
+                self.url = url
                 self.name = volumeName
                 self.blockSize = (UInt64)(fs.f_bsize)
                 self.blocks = fs.f_blocks
@@ -45,7 +49,7 @@ class ViewController: NSViewController, USBDetectorDelegate {
         // フリーブロックの更新
         func upateFreeBlock() {
             var fs: StatFS = StatFS()
-            if statfs(self.path, &fs) == 0 {
+            if statfs(self.url.path, &fs) == 0 {
                 self.freeBlock = fs.f_bfree
             }
         }
@@ -54,12 +58,73 @@ class ViewController: NSViewController, USBDetectorDelegate {
             return self.blocks > 0
         }
         
-        var path: String = "/"
+        var url: URL = URL(fileURLWithPath: "/")
         var name: String = ""
         var blockSize: UInt64 = 0
         var blocks: UInt64 = 0
         var freeBlock: UInt64 = 0
         var dispString: String = ""
+    }
+    
+    class File {
+        
+        var url: URL            // ファイル名を含んだurl
+        var fileName: String    // ファイル名のみ
+        var data: Data          // 読み込んだファイル
+
+        init(_ url: URL, _ fileName: String) {
+            data = Data()
+            self.url = url
+            self.fileName = fileName
+            self.url.appendPathComponent(fileName)
+        }
+        
+        func exists() -> Bool {
+            
+            return FileManager.default.fileExists(atPath: url.path)
+        }
+        
+        func create() -> Bool {
+            return FileManager.default.createFile(atPath: url.path, contents: nil, attributes: nil)
+        }
+        
+        func delete() -> Bool {
+            do {
+                try FileManager.default.removeItem(atPath: url.path)
+            
+            } catch {
+                return false
+            }
+            
+            return true
+        }
+                
+        func read() -> Bool {
+            do {
+                data = try Data(contentsOf: url)
+                
+            } catch {
+                return false
+            }
+
+            return true
+        }
+        
+        func write(_ data: Data) -> Bool {
+            do {
+                try data.write(to: url)
+            } catch {
+                return false
+            }
+            self.data = data
+            return true
+        }
+        
+        func getFirstSectorNumber() -> UInt64 {
+            let attr = try? FileManager.default.attributesOfItem(atPath: url.path)
+            
+            return 0
+        }
     }
     
     var logText: String = ""        // ログテキスト
@@ -89,30 +154,27 @@ class ViewController: NSViewController, USBDetectorDelegate {
         refreshVolumeList()
     }
     
-    @IBAction func onDeleteFiles(_ sender: Any) {
-        
-        // 選択されたボリューム
-        let selectedIndex = volumeList.indexOfSelectedItem
-        
+    
+    func countWriteFiles(_ volume: Volume) -> Int {
         // 存在するファイルの数を数える
         var fileCount: Int = 0
         repeat {
-            ramdomGenerator = SeededGenerator(seed: UInt64(fileCount))
-            let fileName = makeFilenameFromCount(volume: volumeArray[selectedIndex], count: fileCount)
-            if FileManager.default.fileExists(atPath: fileName) == false {
+            let file = File(volume.url, makeFilenameFromCount(fileCount))
+            if file.exists() == false {
                 break
             }
             fileCount += 1
         } while( true )
-
-        // 見つけたファイルだけ削除
-        deleteFileAll(self.volumeArray[selectedIndex], fileCount: fileCount)
+        
+        return fileCount
     }
     
     func deviceAdded(_ device: io_object_t) {
+
     }
     
     func deviceRemoved(_ device: io_object_t) {
+ 
     }
 
     // 非同期のグループ作るよ！！！
@@ -123,7 +185,7 @@ class ViewController: NSViewController, USBDetectorDelegate {
     @IBAction func onButtonAction(_ sender: Any) {
         
         // ファイルカウンタがマイナスなら最初から
-        if self.isInterrupt == false {
+        if self.actionButton.title == "Start" {
             
             // 選択されたボリューム
             let selectedIndex = volumeList.indexOfSelectedItem
@@ -161,15 +223,52 @@ class ViewController: NSViewController, USBDetectorDelegate {
         }
     }
     
-    func startProcessForView(_ selectedIndex: Int) {
-        DispatchQueue.main.async {
-            self.volumeList.isEnabled = false
-            self.actionButton.title = "Stop"
-            self.deleteButton.isEnabled = false
-            self.indicator.floatValue = 0.0
-            self.indicatorSub = ceil(Double(self.volumeArray[selectedIndex].freeBlock) / Double(UInt64(self.MAX_FILE_SIZE)/self.volumeArray[selectedIndex].blockSize)) * 2+1
-            self.indicator.maxValue = self.indicatorSub
-        }
+    @IBAction func onDeleteFiles(_ sender: Any) {
+        // 選択されたボリューム
+        let selectedIndex = volumeList.indexOfSelectedItem
+
+        // 存在するファイルの数を数える
+        let fileCount: Int = countWriteFiles(volumeArray[selectedIndex])
+
+        // 見つけたファイルだけ削除
+        deleteFileAll(self.volumeArray[selectedIndex], fileCount: fileCount)
+        addLog("finished!!")
+        addLog("")
+    }
+    
+    @IBAction func onVerifyFiles(_ sender: Any) {
+        // 選択されたボリューム
+        let selectedIndex = volumeList.indexOfSelectedItem
+        
+        startProcessForView(selectedIndex, indicatorMul: 1)
+        
+        dispatchGroup.enter()
+        dispatchQueue.async {
+            // 存在するファイルの数を数える
+            let fileCount: Int = self.countWriteFiles(self.volumeArray[selectedIndex])
+            
+            self.verifyFileAll(self.volumeArray[selectedIndex], fileCount: fileCount)
+            self.finishIndicatorValue()
+            self.isInterrupt = false
+
+            self.dispatchGroup.leave()
+            self.addLog("finished!!")
+            self.addLog("")
+       }
+       dispatchGroup.notify(queue: .main) {
+           self.finishProcessForView()
+       }
+    }
+
+    func startProcessForView(_ selectedIndex: Int, indicatorMul: Double = 2) {
+        self.volumeList.isEnabled = false
+        self.actionButton.title = "Stop"
+        self.deleteButton.isEnabled = false
+        self.verifyButton.isEnabled = false
+        self.refreshButton.isEnabled = false
+        self.indicator.floatValue = 0.0
+        self.indicatorSub = ceil(Double(self.volumeArray[selectedIndex].freeBlock) / Double(UInt64(MAX_FILE_SIZE)/self.volumeArray[selectedIndex].blockSize)) * indicatorMul+1
+        self.indicator.maxValue = self.indicatorSub
     }
     
     func finishProcessForView() {
@@ -178,6 +277,8 @@ class ViewController: NSViewController, USBDetectorDelegate {
             self.actionButton.isEnabled = true
             self.actionButton.title = "Start"
             self.deleteButton.isEnabled = true
+            self.verifyButton.isEnabled = true
+            self.refreshButton.isEnabled = true
         }
     }
     
@@ -269,6 +370,12 @@ class ViewController: NSViewController, USBDetectorDelegate {
             volumeList.addItem(withTitle: volume.dispString)
         }
         
+        // 開始可能か？
+        let isAction = volumeArray.count > 0
+        actionButton.isEnabled = isAction
+        deleteButton.isEnabled = isAction
+        verifyButton.isEnabled = isAction
+
 /*
         let files = FileManager.default.enumerator(atPath: "/Volumes")
         while let file = files?.nextObject() {
@@ -278,19 +385,19 @@ class ViewController: NSViewController, USBDetectorDelegate {
     }
     
     var ramdomGenerator = SeededGenerator()    // シード指定可のランダムジェネレータ
-    let MAX_FILE_SIZE = 2*1024*1024*1024/16    // 作成ファイルの最大サイズ
     
     // 乱数を使用してテンポラリのファイル名をフルパスで作成する
-    func makeFilenameFromCount(volume: Volume, count: Int) -> String {
+    func makeFilenameFromCount(_ count: Int) -> String {
+/*
         var fileName: String = volume.path
             
         if fileName.hasSuffix("/") == false {
             fileName += "/"
         }
-        fileName += String(format: "%08X", count)
-        fileName += ".tsm"
+        fileName += String(format: "%08X", count) + ".tsm"
+ */
 
-        return fileName
+        return String(format: "%08d", count) + ".tsm"
     }
     
     // ストレージの残りを見て最大ファイルサイズを計算する
@@ -312,7 +419,7 @@ class ViewController: NSViewController, USBDetectorDelegate {
 // 要素ごとに乱数を入れると重すぎて死にます
 /*
         writeArray.withUnsafeMutableBufferPointer { buffer in
-            for i in 0..<MAKE_MAX_FILE_SIZE/MemoryLayout<UInt64>.size {
+            for i in 0..<size/MemoryLayout<UInt64>.size {
                 buffer[i] = UInt64.random(in: UInt64.min...UInt64.max, using: &randomGenerator)
             }
         }
@@ -320,15 +427,9 @@ class ViewController: NSViewController, USBDetectorDelegate {
         return Data(bytes: &writeArray, count: writeArray.count*MemoryLayout<UInt64>.size)
     }
     
-    func deleteExistsFile(_ fileName: String) -> Bool {
-        if FileManager.default.fileExists(atPath: fileName) {
-            do {
-                try FileManager.default.removeItem(atPath: fileName)
-                
-            } catch {
-                 return false
-            }
-
+    func deleteExistsFile(_ file: File) -> Bool {
+        if file.exists() == true {
+            return file.delete()
         }
         return true
     }
@@ -336,8 +437,8 @@ class ViewController: NSViewController, USBDetectorDelegate {
     func writeFile(_ volume: Volume, count: Int) -> (time: TimeInterval, writeSize: Int) {
         ramdomGenerator = SeededGenerator(seed: UInt64(count))
         
-        // ファイル名を先に作る
-        let fileName = makeFilenameFromCount(volume: volume, count: count)
+        // ファイルを先に作る
+        let file = File(volume.url, makeFilenameFromCount(count))
 
         // make file size
         let fileSize = calcMaxFileSize(volume: volume)
@@ -350,12 +451,12 @@ class ViewController: NSViewController, USBDetectorDelegate {
         let writeData = createWriteData(initData: rnd, size: fileSize)
 
         // ファイルがあったらスキップ
-        if FileManager.default.fileExists(atPath: fileName) {
+        if file.exists() {
             return (Double(fileSize)/1000.0/1000.0, fileSize)
         }
 /*
         // ファイルがあったら削除
-        if deleteExistsFile(fileName) == false {
+        if deleteExistsFile(file) == false {
             addLog("can't delete exists file (" + fileName + ")")
             return (0.0, 0)
         }
@@ -364,21 +465,17 @@ class ViewController: NSViewController, USBDetectorDelegate {
         let startDate = Date()
         
         // create & write file
-        let result = FileManager.default.createFile(atPath: fileName, contents: nil, attributes: nil)
-        if result == true {
-            do {
-                let url = URL(fileURLWithPath: fileName)
-                try writeData.write(to: url)
-            } catch {
-                addLog("write file error! \(fileName)")
-                deleteExistsFile(fileName)
+        if file.create() == true {
+            if file.write(writeData) == false {
+                addLog("write file error! \(file.fileName)")
+                file.delete()
                 return (0.0, 0)
             }
             let dt = Date().timeIntervalSince(startDate)
             return (Double(fileSize)/dt/1000.0/1000.0, fileSize)
 
         } else {
-            addLog("can't create file: \(fileName)")
+            addLog("can't create file: \(file.fileName)")
         }
         
         return (0.0, 0)
@@ -397,7 +494,7 @@ class ViewController: NSViewController, USBDetectorDelegate {
                 inclimentIndicatorValue()
                 totalWriteSize += result.writeSize
                 averageWriteTime = (averageWriteTime + result.time)/2.0
-                setStateText( process: "write files", totalSize: totalWriteSize, time: result.time, count: fileCount)
+                setStateText( process: "write bytes", totalSize: totalWriteSize, time: result.time, count: fileCount)
             } else {
                 break
             }
@@ -410,10 +507,11 @@ class ViewController: NSViewController, USBDetectorDelegate {
     }
     
     func verifyFile(_ volume: Volume, count: Int) -> (time: TimeInterval, verifySize: Int) {
+        
         ramdomGenerator = SeededGenerator(seed: UInt64(count))
         
-        // ファイル名を先に作る
-        let fileName = makeFilenameFromCount(volume: volume, count: count)
+        // ファイルを先に作る
+        let file = File(volume.url, makeFilenameFromCount(count))
   
         // create compare data
         let compareData = UInt64.random(in: UInt64.min...UInt64.max, using: &ramdomGenerator)
@@ -421,54 +519,51 @@ class ViewController: NSViewController, USBDetectorDelegate {
         // 開始時間の取得
         let startDate = Date()
 
-        var readData: Data
-        do {
-            let url = URL(fileURLWithPath: fileName)
-            readData = try Data(contentsOf: url)
-        } catch {
-            addLog("read file error! \(fileName)")
-            return (0.0, 0)
-        }
-
-        var dt = Date().timeIntervalSince(startDate)
-        dt = Double(readData.count)/dt/1000/1000
-
-        // メモリデータの比較
-        let size: Int = readData.withUnsafeBytes { (rptr: UnsafePointer<UInt64>) -> Int in
-            var i: Int = 0
-            while i < readData.count/MemoryLayout<UInt64>.size {
-                if     rptr[i+0]  != compareData
-                    || rptr[i+1]  != compareData
-                    || rptr[i+2]  != compareData
-                    || rptr[i+3]  != compareData
-                    || rptr[i+4]  != compareData
-                    || rptr[i+5]  != compareData
-                    || rptr[i+6]  != compareData
-                    || rptr[i+7]  != compareData
-                    || rptr[i+8]  != compareData
-                    || rptr[i+9]  != compareData
-                    || rptr[i+10] != compareData
-                    || rptr[i+11] != compareData
-                    || rptr[i+12] != compareData
-                    || rptr[i+13] != compareData
-                    || rptr[i+14] != compareData
-                    || rptr[i+15] != compareData
-                {
-                    return readData.count-i*MemoryLayout<UInt64>.size
-                }
-                i += 16
+        let result = autoreleasepool { () -> (Double, Int) in // コレないとメモリプールが溜まって死亡
+            if file.read() == false {
+                addLog("read file error! \(file.fileName)")
+                return (0.0, 0)
             }
-            
-            return readData.count
+
+            var dt = Date().timeIntervalSince(startDate)
+            dt = Double(file.data.count)/dt/1000/1000
+
+            // メモリデータの比較
+            let size: Int = file.data.withUnsafeBytes { (rptr: UnsafePointer<UInt64>) -> Int in
+                var i: Int = 0
+                while i < file.data.count/MemoryLayout<UInt64>.size {
+                    if     rptr[i+0]  != compareData
+                        || rptr[i+1]  != compareData
+                        || rptr[i+2]  != compareData
+                        || rptr[i+3]  != compareData
+                        || rptr[i+4]  != compareData
+                        || rptr[i+5]  != compareData
+                        || rptr[i+6]  != compareData
+                        || rptr[i+7]  != compareData
+                        || rptr[i+8]  != compareData
+                        || rptr[i+9]  != compareData
+                        || rptr[i+10] != compareData
+                        || rptr[i+11] != compareData
+                        || rptr[i+12] != compareData
+                        || rptr[i+13] != compareData
+                        || rptr[i+14] != compareData
+                        || rptr[i+15] != compareData
+                    {
+                        return file.data.count-i*MemoryLayout<UInt64>.size
+                    }
+                    i += 16
+                }
+                
+                return file.data.count
+            }
+            if size < file.data.count {
+                addLog("verify file error! \(file.fileName)")
+                return (dt, -file.data.count)
+            }
+
+            return (dt, file.data.count)
         }
-        if size < readData.count {
-            addLog("verify file error! \(fileName)")
-            readData.removeAll(keepingCapacity: false)
-            return (dt, -size)
-        }
-        
-        readData.removeAll(keepingCapacity: false)
-        return (dt, size)
+        return result
     }
     
     func verifyFileAll(_ volume: Volume, fileCount: Int) {
@@ -489,10 +584,10 @@ class ViewController: NSViewController, USBDetectorDelegate {
                 if result.verifySize <= 0 {
                     // 比較エラー
                     totalErrorSize += -result.verifySize
-                    setStateText( process: "verify error files", totalSize: totalErrorSize, time: result.time, count: i)
+                    setStateText( process: "verify failure bytes", totalSize: totalErrorSize, time: result.time, count: i)
                 } else {
                     totalVerifySize += result.verifySize
-                    setStateText( process: "verify files", totalSize: totalVerifySize, time: result.time, count: i)
+                    setStateText( process: "verify success bytes", totalSize: totalVerifySize, time: result.time, count: i)
                 }
             }
             averageReadTime = (averageReadTime + result.time)/2.0
@@ -503,15 +598,10 @@ class ViewController: NSViewController, USBDetectorDelegate {
     }
     
     func deleteFile(_ volume: Volume, count: Int) -> String {
-        ramdomGenerator = SeededGenerator(seed: UInt64(count))
-        
-        let fileName = makeFilenameFromCount(volume: volume, count: count)
+        let file = File(volume.url, makeFilenameFromCount(count))
 
-        do {
-            try FileManager.default.removeItem(atPath: fileName)
-            
-        } catch {
-            return fileName
+        if file.delete() == false {
+            return file.fileName
         }
 
         return ""
